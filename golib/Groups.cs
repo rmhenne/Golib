53a16c85b791a11d77030a6278216a69942d1693
Randal Henne <rmhenne@gmail.com>
2013-08-16 14:38:26 -0700

    more groups logic

--- golib/Groups.cs
+++ golib/Groups.cs
@@ -8,37 +8,79 @@ namespace golib
 {
     public class Group
     {
-        private int d_row, d_col;
+
+        // Bounding Box
+        private Tuple<int,int> d_lowerLeftBound,
+            d_upperRightBound;
         private List<Tuple<int, int>> d_members,
             d_liberties;
         CellState d_color;
+        private int[,] d_lattice;
+
 
         public Group( int row, int col, CellState cs )
         {
-            d_row = row;
-            d_col = col;
+            d_lowerLeftBound  = Tuple.Create(row, col);
+            d_upperRightBound = Tuple.Create(row, col);
+            
             d_color = cs;
+            d_members = new List<Tuple<int, int>>();
+            AddCore(row, col);
         }
+
+        private void AdjustBoundingBox(int row, int col)
+        {
+            int rowLow = Math.Min(d_lowerLeftBound.Item1, row),
+                rowHi = Math.Max(d_upperRightBound.Item1, row),
+                colLow = Math.Min(d_lowerLeftBound.Item2, col),
+                colHi = Math.Max(d_upperRightBound.Item2, col);
+
+            d_lowerLeftBound = Tuple.Create(rowLow, colLow);
+            d_upperRightBound = Tuple.Create(rowHi, colHi);
+        }
+
+        private void AddCore(int row, int col)
+        {
+            AdjustBoundingBox(row, col);
+
+            d_members.Add(Tuple.Create(row, col));
+
+        }
+
 
         public void Add( int row, int col )
         {
-            if (row <= d_row && col <= d_col)
-            {
-                d_row = row;
-                d_col = col;
-            }
-
-            d_members.Add(Tuple.Create(row, col));
-      
+            AddCore(row, col);
         }
 
 
         public void Add(List<Tuple<int,int>> stones )
         {
+            foreach (var stone in stones)
+            {
+                AddCore(stone.Item1, stone.Item2);
+            }
         }
 
+        private bool CheckConnectedness()
+        {
+            throw new NotImplementedException("CheckConnectedness");
+        }
+
+        private void PlacePiecesOnLattice()
+        {
+            int width = d_upperRightBound.Item2 - d_lowerLeftBound.Item2 + 1,
+                height = d_upperRightBound.Item1 - d_lowerLeftBound.Item1 + 1;
+
+            d_lattice = new int[height, width];
+
+            foreach (var stone in d_members)
+            {
+
+            }
 
 
+        }
     }
 
 
