8042f167b7028378b900e26b9e23a98cfc2d758b
Randal Henne <rmhenne@gmail.com>
2013-08-22 13:40:32 -0700

    Added some contracts and tests

---
+++ golib/GoTop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golib
+{
+    class GoTop
+    {
+        private BoardModel d_boardModel;
+        private Groups d_groups;
+
+        public GoTop()
+        {
+            d_boardModel = new BoardModel(19);
+            d_groups = new Groups();
+
+            Contract.Ensures(d_groups.count == 0);
+        }
+    }
+}
